7c0c468543345f094c872187da4a501992f16e58
PhromphiriyBuMail-Tech <phromphiriya.leks@bumail.net>
2026-08-25 11:04:30 +0700

    Add lab02 part A

--- Lab02/Program.cs
+++ Lab02/Program.cs
@@ -11,8 +11,29 @@ namespace Lab02
     internal class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
+            
+        string bossName = "Kirin";
+        char rank = 'S';
+        int level = 7;
+        int maxHp = 240;
+        int currentHp = 175;
+        float attackPower = 42.5f;
+        double critMultiplier = 1.75;
+        bool isBoss = true;
+   
+            Console.WriteLine("===== boss status: initial =====");
+            Console.WriteLine($"Name: {BossName}");
+            Console.WriteLine($"Rank: {rank}");
+            Console.WriteLine($"Hp: {currentHp} / {maxhp}");
+            Console.WriteLine($"Attack power: {attackPower}");
+            Console.WriteLine($"Crit Multiplier: {critMultiplier}");
+            Console.WriteLine($"Is boss: {isBoss}");
+
+            Console.WriteLine();
+
+            int hpPercent = currentHp * 100 / maxHp;
+            Console.WriteLine();
+            Console.
         }
     }
 }
